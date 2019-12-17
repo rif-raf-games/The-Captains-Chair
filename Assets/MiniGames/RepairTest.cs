98e917e82e1cfc8b99324aa1d2b732ed7fbe5d29
MikeRifRaf <m.oneil@rifrafgames.com>
2019-12-17 16:29:51 -0700

    More Repair game updates.

---
+++ Assets/MiniGames/RepairTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairTest : MonoBehaviour
+{
+    public Transform AnchorPoint;
+    public Transform Piece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,100,100), "feh"))
+        {
+            Piece.transform.position = AnchorPoint.transform.position;
+        }
+    }
+}
