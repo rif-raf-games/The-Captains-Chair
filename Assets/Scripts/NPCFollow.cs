201c1deff09dad6178c490d59cda61322da947e5
MikeRifRaf <m.oneil@rifrafgames.com>
2019-12-20 13:07:23 -0700

    Added articy/ui stuff to the astroid scene.  Also a first pass npc follower.

---
+++ Assets/Scripts/NPCFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCFollow : MonoBehaviour
+{
+    public CCPlayer ToFollow;
+    private NavMeshAgent NavMeshAgent;
+
+    private void Start()
+    {
+        NavMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+    void LateUpdate()
+    {
+        NavMeshAgent.SetDestination(ToFollow.transform.position);
+    }
+}
