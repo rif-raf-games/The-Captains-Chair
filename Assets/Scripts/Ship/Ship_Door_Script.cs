14a1ffa06338ea1974248a575dfd99b2cab885f7
Pat Skelton <p.skelton@rifrafgames.com>
2020-01-28 17:11:19 -0500

    Majestic Updates

--- Assets/Scripts/Ship/Ship_Door_Script.cs
+++ Assets/Scripts/Ship/Ship_Door_Script.cs
@@ -3,15 +3,18 @@
 public class Ship_Door_Script : MonoBehaviour {
 
     public GameObject Trigger;
-    public GameObject DoorTop;
-    public GameObject DoorBottom;
+    public GameObject Door_01;
+    public GameObject Door_02;
+    public GameObject Door_03;
 
-    Animator DoorTopAnimation;
-    Animator DoorBottomAnimation;
+    Animator Door_01_Animation;
+    Animator Door_02_Animation;
+    Animator Door_03_Animation;
 
     void Start() {
-        DoorTopAnimation = DoorTop.GetComponent<Animator > ();
-        DoorBottomAnimation = DoorBottom.GetComponent<Animator > ();
+        Door_01_Animation = Door_01.GetComponent<Animator > ();
+        Door_02_Animation = Door_02.GetComponent<Animator > ();
+        Door_03_Animation = Door_03.GetComponent<Animator > ();
     }
 
     void OnTriggerEnter(Collider coll) {
@@ -27,8 +30,9 @@ public class Ship_Door_Script : MonoBehaviour {
 
     void SlideDoors(bool state)
     {
-        DoorTopAnimation.SetBool ("slide", state);
-        DoorBottomAnimation.SetBool ("slide", state);
+        Door_01_Animation.SetBool ("slide", state);
+        Door_02_Animation.SetBool ("slide", state);
+        Door_03_Animation.SetBool ("slide", state);
 
         // true = open, false = close        
         if (state == true) SoundFXPlayer.Play("Door_Open");
