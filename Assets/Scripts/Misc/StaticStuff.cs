6d5215e65fff55990606ab4662cb97a038020c1a
msrifkin <m.rifkin@rifrafgames.com>
2020-06-11 17:01:01 -0400

    prop asset pack, update to maj prefab and functional transitions betwenn plaza and iminigame scenes, also aliens in plaza

--- Assets/Scripts/Misc/StaticStuff.cs
+++ Assets/Scripts/Misc/StaticStuff.cs
@@ -13,7 +13,7 @@ using UnityEngine.SceneManagement;
 
 static public class StaticStuff 
 {
-    static string DEBUG_SCENE_TO_LOAD = "Ep1.Exchange";
+    static string DEBUG_SCENE_TO_LOAD = "Ep1.Plaza";
     public enum eOrientation { LANDSCAPE, PORTRAIT };
 
     static public void SetOrientation(eOrientation orientation, string screenName)
