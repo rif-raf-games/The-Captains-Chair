fe35a218c2550b2328ef3ee286db399f4b133043
Brent Boylen <b.boylen@rifrafgames.com>
2021-11-15 16:10:15 -0500

    Adding Unity IAP package 4.1.1

---
+++ Assets/Scripts/UnityPurchasing/generated/AppleStoreKitTestTangle.cs
@@ -0,0 +1,18 @@
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleStoreKitTestTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("oVtXV11TVlXUV1dW5Fa2aqe+vzRb7W7GPVCSqluzwwabx57sc6IOn11mX1BVA1JQRVQDBWdBZkNQX3zQY2RlYwxBW2JmZmVkYWdhY2RlYwyxObZNsFs+m3M4lSBeoxxd+Cwiatq3J40cE6V7T8BVANfwErgjIQ0FWl4FIjkkMx0/ImddZl9QVQNSUEVnR2ZZUFUDUl1aXgUiOSQzHT8iZ1JVWl4FIjkkMx0/ImdHZllQVQNSF/MoX9vsa2ubJgGOu7mDjCGexVIFIjkkMx0/ImZIQVtkZmZiZmdnYeuWgEsWyAHvbsYhIASE/h0mOO4xHtChW1dfV0BeBSI5JDMdPyJm1FeZMmo71g9l2pxgUHxHTdsfmF6pMHojsBrnRIWrtUkIWyvuDoH2KrIpNiBhBLqp+k8LWkN53Fmj356BfUuFIEakpj+6mzTPJ6823NIebpLr3UtFV1epUlNmVVdXqWZYUFUDS1lXHcXtXK73d2ieN49ynghJI2iMxY1dbxqdGTiW5Qnhw+zv/gscIv25OYGyLv75/jvxl4w+ezCamX9dBnq1ZtRVImbUVAr2VVRXVFRXV2ZbUF8iZ0dmWVBVA1JcWl4FIjkkMx0/Ir3hFtdr0WsmB4eSC9vziik8DkyIZglnR2ZZUFUDUlVaXgUiOSQzHT/PreXcA8m5S37uCXyHJcljEWHtEHzQHtChW1dXXVNWZglnR2ZZUFUDV6lSUlVUVNJmQFBVA0tzV1epUlp0ZltQX3zQHtChW1dXV1NWVdRXWTFz7/06ZTP7jpTMLh5Zv5qXcVuQSrlf3i4zmNek2ySxUUw4cEShZOdUAwVnQWZDUF980B7QoVtXX1dAXpL/XeWZNFmt4uqhjdIIfr0RW6e7mS76C+xoCQZ9Cn/ai5SBfEGJdOKWEX4dvQyXfV8AevP8X1UtZGeKvjX9wcx9lXyOhuS3gwxbb005MX7/XFpeBSI5JDMdPyJnR2ZZUFUDUl1nTMfW7+mzWp1q7VgSNHCsUn/gH5StsAlEl1RVV1ZX9W1mb2ZZUFUD3fqjyeZfbvU4LZ1wBlxMt9+HJCBE1YDRLjvP6wjlXLbOmKC8ldg7P1Zm1FdcVNRXV1aNKcZqeiVZjcFw0y4emJaTREc8WVr4eVOcOSwyKXZmXFBefVBXU1NRVVVmW1BffNAe0Ei6fcdT8WsZ");
+        private static int[] order = new int[] { 31,23,11,7,17,24,17,10,11,21,17,32,12,36,39,43,34,28,43,20,26,31,31,42,32,33,42,29,36,35,31,32,43,33,37,40,36,37,40,42,41,42,42,43,44 };
+        private static int key = 86;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
--- Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
+++ Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
@@ -3,9 +3,9 @@
 namespace UnityEngine.Purchasing.Security {
     public class GooglePlayTangle
     {
-        private static byte[] data = System.Convert.FromBase64String("56NHuY7EiqhOoKWCDuZjA2H8jIwyhVI8qMKv7Tv8EY9Wwi4Rss2P5YU3tJeFuLO8nzP9M0K4tLS0sLW2ROVo0m3aJZtRB6pwzuczQak5rPRzbMNPBN0yeAJjDlqD55YEdqIWmux5nUkm7+WPqwm3TCmMuCRGM8xYWYlG95l1eNwp+Pu9w4SfTWoDROAzkgStyDLUw/L6l8R2wIiBxXg0OWShqs4sESs+1uhW8w1E+S3SDiFhM5RC3mYl8yiUTcRZzvIYuJzqqaI3tLq1hTe0v7c3tLS1Oods6iAq27OGO0ZxYHAALWXeA1Uk/SlKuBD1LtwXiTdsZdU/LbEeBdSiCvPAQKWCm1VFJsIUqgjz/d2ZM+4PHknlocYIgZVBFDnt8Le2tLW0");
-        private static int[] order = new int[] { 5,2,5,8,7,13,12,8,8,12,13,11,13,13,14 };
-        private static int key = 181;
+        private static byte[] data = System.Convert.FromBase64String("yXv428n0//DTf7F/DvT4+Pj8+fqr7wv1wojG5ALs6c5Cqi9PLbDAwP/Kdwo9LDxMYSmSTxlosWUG9Fy5FcUKu9U5NJBltLfxj8jTASZPCKw/II8DSJF+NE4vQhbPq9pIOu5a1ijt5oJgXWdymqQav0EItWGeQm0tCKkkniGWadcdS+Y8gqt/DeV14Lh7+Pb5yXv48/t7+Pj5dssgpmxml87XGQlqjljmRL+xkdV/okNSBantfskecOSO46F3sF3DGo5iXf6Bw6l/3kjhhH6Yj76224g6jMTNiTR4dWKQW8V7ICmZc2H9UkmY7ka/jAzpoDXRBWqjqcPnRfsAZcD0aAp/gBR/2A6SKmm/ZNgBiBWCvlT00Kbl7opEzdkNWHWhvPv6+Pn4");
+        private static int[] order = new int[] { 0,5,11,12,7,7,8,7,9,11,12,12,13,13,14 };
+        private static int key = 249;
 
         public static readonly bool IsPopulated = true;
 
