a00469ad990d9d915394e84a5287f284a0ea1a0c
Brent Boylen <b.boylen@rifrafgames.com>
2022-04-28 10:32:02 -0400

    multiple settings files to bring everything to 2.1.2o fixes for credits integrated Mo's fixes from last check in

--- Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
+++ Assets/Scripts/UnityPurchasing/generated/GooglePlayTangle.cs
@@ -3,9 +3,9 @@
 namespace UnityEngine.Purchasing.Security {
     public class GooglePlayTangle
     {
-        private static byte[] data = System.Convert.FromBase64String("yXv428n0//DTf7F/DvT4+Pj8+fqr7wv1wojG5ALs6c5Cqi9PLbDAwP/Kdwo9LDxMYSmSTxlosWUG9Fy5FcUKu9U5NJBltLfxj8jTASZPCKw/II8DSJF+NE4vQhbPq9pIOu5a1ijt5oJgXWdymqQav0EItWGeQm0tCKkkniGWadcdS+Y8gqt/DeV14Lh7+Pb5yXv48/t7+Pj5dssgpmxml87XGQlqjljmRL+xkdV/okNSBantfskecOSO46F3sF3DGo5iXf6Bw6l/3kjhhH6Yj76224g6jMTNiTR4dWKQW8V7ICmZc2H9UkmY7ka/jAzpoDXRBWqjqcPnRfsAZcD0aAp/gBR/2A6SKmm/ZNgBiBWCvlT00Kbl7opEzdkNWHWhvPv6+Pn4");
-        private static int[] order = new int[] { 0,5,11,12,7,7,8,7,9,11,12,12,13,13,14 };
-        private static int key = 249;
+        private static byte[] data = System.Convert.FromBase64String("wFWxZQrDyaOHJZtgBaCUCGof4HRfQO9jKPEeVC5PInavy7ooWo46tq63eWkK7jiGJN/R8bUfwiMyZcmNH7hu8koJ3wS4Yeh14t40lLDGhY6pG5i7qZSfkLMf0R9ulJiYmJyZmgLwO6UbQEn5EwGdMin4jibf7GyJG5iWmakbmJObG5iYmRarQMYMBvcfviiB5B74797Wu+ha7KSt6VQYFXWlatu1WVTwBdTXke+os2FGL2jMy49rlaLopoRijImuIspPL03QoKBIjYbiAD0HEvrEet8haNUB/iINTZ+qF2pdTFwsAUnyL3kI0QVmlDzZaMlE/kH2Cbd9K4Zc4ssfbYUVgNgeqX4QhO6DwRfQPaN67gI9nuGjyeokrbltOBXB3JuamJmY");
+        private static int[] order = new int[] { 13,7,9,6,13,10,7,7,12,10,13,11,12,13,14 };
+        private static int key = 153;
 
         public static readonly bool IsPopulated = true;
 
